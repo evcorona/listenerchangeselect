e911dca70dadd7805a16d723f204fca7e4be19eb
veroxcrown <eldav.corona@gmail.com>
2020-12-02 18:30:14 -0600

    Uploading complete exercise

---
+++ js/main.js
@@ -0,0 +1,237 @@
+let personArray = [
+  {
+    "_id": "5fc550e1c9c6046f9d108e02",
+    "isActive": false,
+    "balance": "$3,938.67",
+    "age": 35,
+    "name": "Campos Schultz",
+    "gender": "male",
+    "email": "camposschultz@overfork.com",
+    "phone": "+1 (962) 497-3378",
+    "type": "Koder",
+    "favoriteFruit": "kiwi",
+    "pictureUrl": "https://picsum.photos/id/100/600"
+  },
+  {
+    "_id": "5fc550e1d6fafd4f83243ea5",
+    "isActive": true,
+    "balance": "$2,249.69",
+    "age": 12,
+    "name": "Marcy Rios",
+    "gender": "female",
+    "email": "marcyrios@overfork.com",
+    "phone": "+1 (802) 418-2723",
+    "type": "Mentor",
+    "favoriteFruit": "apple",
+    "pictureUrl": "https://picsum.photos/id/101/600"
+  },
+  {
+    "_id": "5fc550e1b641b3f753407166",
+    "isActive": false,
+    "balance": "$1,236.20",
+    "age": 19,
+    "name": "Regina Montgomery",
+    "gender": "female",
+    "email": "reginamontgomery@overfork.com",
+    "phone": "+1 (878) 433-3379",
+    "type": "Koder",
+    "favoriteFruit": "apple",
+    "pictureUrl": "https://picsum.photos/id/102/600"
+  },
+  {
+    "_id": "5fc550e1b2a5ef8b0b048c84",
+    "isActive": false,
+    "balance": "$3,516.66",
+    "age": 15,
+    "name": "Debora Wilder",
+    "gender": "female",
+    "email": "deborawilder@overfork.com",
+    "phone": "+1 (818) 537-3416",
+    "type": "Koordinator",
+    "favoriteFruit": "mango",
+    "pictureUrl": "https://picsum.photos/id/103/600"
+  },
+  {
+    "_id": "5fc550e153b087a36752e2b0",
+    "isActive": true,
+    "balance": "$3,320.58",
+    "age": 25,
+    "name": "Mckay Dillon",
+    "gender": "male",
+    "email": "mckaydillon@overfork.com",
+    "phone": "+1 (861) 439-3553",
+    "type": "Mentor",
+    "favoriteFruit": "strawberry",
+    "pictureUrl": "https://picsum.photos/id/104/600"
+  },
+  {
+    "_id": "5fc550e184b6e2150f833160",
+    "isActive": true,
+    "balance": "$3,887.48",
+    "age": 18,
+    "name": "Duncan Rogers",
+    "gender": "male",
+    "email": "duncanrogers@overfork.com",
+    "phone": "+1 (898) 483-3472",
+    "type": "Koordinator",
+    "favoriteFruit": "mango",
+    "pictureUrl": "https://picsum.photos/id/125/600"
+  },
+  {
+    "_id": "5fc550e1f440854873851dd1",
+    "isActive": false,
+    "balance": "$1,598.80",
+    "age": 36,
+    "name": "Miller Campos",
+    "gender": "male",
+    "email": "millercampos@overfork.com",
+    "phone": "+1 (905) 417-2415",
+    "type": "Koordinator",
+    "favoriteFruit": "kiwi",
+    "pictureUrl": "https://picsum.photos/id/106/600"
+  },
+  {
+    "_id": "5fc550e1567efabb89397058",
+    "isActive": true,
+    "balance": "$1,955.62",
+    "age": 39,
+    "name": "Larsen Valenzuela",
+    "gender": "male",
+    "email": "larsenvalenzuela@overfork.com",
+    "phone": "+1 (939) 449-2471",
+    "type": "Koordinator",
+    "favoriteFruit": "banana",
+    "pictureUrl": "https://picsum.photos/id/107/600"
+  },
+  {
+    "_id": "5fc550e19dd6acf4895166ee",
+    "isActive": true,
+    "balance": "$1,372.63",
+    "age": 39,
+    "name": "Kristen Battle",
+    "gender": "female",
+    "email": "kristenbattle@overfork.com",
+    "phone": "+1 (969) 430-2022",
+    "type": "Staff",
+    "favoriteFruit": "banana",
+    "pictureUrl": "https://picsum.photos/id/108/600"
+  },
+  {
+    "_id": "5fc550e1ed1a0866331fb205",
+    "isActive": true,
+    "balance": "$2,428.07",
+    "age": 36,
+    "name": "Candace Lawrence",
+    "gender": "female",
+    "email": "candacelawrence@overfork.com",
+    "phone": "+1 (944) 522-2722",
+    "type": "Staff",
+    "favoriteFruit": "kiwi",
+    "pictureUrl": "https://picsum.photos/id/109/600"
+  },
+  {
+    "_id": "5fc550e1f3ddb61b7be83dea",
+    "isActive": false,
+    "balance": "$1,547.53",
+    "age": 32,
+    "name": "Bright Garcia",
+    "gender": "male",
+    "email": "brightgarcia@overfork.com",
+    "phone": "+1 (960) 532-2477",
+    "type": "Koordinator",
+    "favoriteFruit": "kiwi",
+    "pictureUrl": "https://picsum.photos/id/110/600"
+  },
+  {
+    "_id": "5fc550e1c5fd86221cc28411",
+    "isActive": true,
+    "balance": "$1,240.39",
+    "age": 27,
+    "name": "Hope Hoover",
+    "gender": "female",
+    "email": "hopehoover@overfork.com",
+    "phone": "+1 (828) 503-3344",
+    "type": "Koder",
+    "favoriteFruit": "strawberry",
+    "pictureUrl": "https://picsum.photos/id/111/600"
+  },
+  {
+    "_id": "5fc550e18cd4d758d5a1b4c4",
+    "isActive": false,
+    "balance": "$1,309.06",
+    "age": 24,
+    "name": "Josephine Huff",
+    "gender": "female",
+    "email": "josephinehuff@overfork.com",
+    "phone": "+1 (832) 486-2801",
+    "type": "Mentor",
+    "favoriteFruit": "strawberry",
+    "pictureUrl": "https://picsum.photos/id/112/600"
+  },
+  {
+    "_id": "5fc550e1fce533cad2c89fc0",
+    "isActive": true,
+    "balance": "$2,646.14",
+    "age": 22,
+    "name": "Mercado Hart",
+    "gender": "male",
+    "email": "mercadohart@overfork.com",
+    "phone": "+1 (885) 598-2027",
+    "type": "Koder",
+    "favoriteFruit": "apple",
+    "pictureUrl": "https://picsum.photos/id/113/600"
+  }
+]
+/*
+1.- Investigar el evento change 
+2.- Crear en HTML un elemento tipo select, con las opciones Mentor, Koder, Staff
+3.- Crear en HTML un párrafo, el texto de este párrafo cambiará dependiendo de la opción que se elija en el select:    
+  Mentor => "seleccionaste mentor"     
+  Koder => "seleccionaste Koder"    
+  Staff => "seleccionaste staff"
+
+Una vez logrado eso, ese select debe funcionar como un criterio de filtro. 
+Deberán crear una tabla, la cual se llenará con aquellos objetos de personArray cuyo tipo coincida con el seleccionado en el select
+*/
+
+//Listener activa cada que se cambia select
+document.getElementById("mySelector").addEventListener('change', (event) => {
+  if (event.target.value !== "") {
+    document.getElementById("selectionAlert").textContent = `Seleccionaste ${event.target.value}`
+    let typeSelected = event.target.value
+    document.getElementById("selectedData").innerHTML = ""
+    fillFilteredData(personArray, typeSelected)
+  }
+  else {
+    document.getElementById("selectionAlert").textContent = ``
+    document.getElementById("selectedData").innerHTML = "No data selected!"
+  }
+})
+
+/*Llenar datos a una tabla*/
+const fillFilteredData = (BDdata, selection) => {
+  let filteredData = BDdata.filter(data => { return data.type === selection })
+  /*iteramos dentro de nuestro array de datos*/
+  filteredData.forEach(data => { /*por cada objeto de datos*/
+    /*creamos una fila que será inyectada en la tabla*/
+    let dataRow = document.createElement("tr")
+    /*creamos las celdas que serán inyectadas en la fila*/
+    let cellData1 = document.createElement("td")
+    let cellData2 = document.createElement("td")
+    let cellData3 = document.createElement("td")
+    /*creamos los nodos de texto que serán inyectados en cada respectiva celda*/
+    let textData1 = document.createTextNode(data.name)
+    let textData2 = document.createTextNode(data.email)
+    let textData3 = document.createTextNode(data.type)
+    /*inyectamos los textos en las celdas correspondientes*/
+    cellData1.appendChild(textData1)
+    cellData2.appendChild(textData2)
+    cellData3.appendChild(textData3)
+    /*inyectamos las celdas que ya contienen texto dentro de la fila*/
+    dataRow.appendChild(cellData1)
+    dataRow.appendChild(cellData2)
+    dataRow.appendChild(cellData3)
+    /*inyectamos la fila en el elemento correspondiente*/
+    document.getElementById("selectedData").appendChild(dataRow)
+  })
+}
